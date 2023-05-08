3e8273b65f4e70d90eb9997d0a806093080db279
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2023-05-08 20:42:19 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+
+int main() {
+
+    vector<Student> students;
+
+
+// menu
+
+    print_menu(students);
+
+// Simple interface that takes the path of the file from the user
+
+
+//    Student s1(1, "Youssef", 3.0, "IS");
+//    s1.print_info();
+
+//    students[0].print_info();
+//    students[1].print_info();
+
+//    for (int i = 0; i < 10; i++)
+//    {
+//        students[i].print_info();
+//    }
+
+
+    return 0;
+}
